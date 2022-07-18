821e2e4b6ee317491efac6cd05c757d08e35fa09
Miguel Tornero <a00820449@itesm.mx>
2022-07-18 16:47:07 -0500

    added table output

--- Proyecto02/ATMQueue.py
+++ Proyecto02/ATMQueue.py
@@ -1,9 +1,22 @@
 import random
 
 def get_time_string(n):
-    hours = n / 60
+    hours = n // 60
     mins = n % 60
-    return "{}:{}".format(hours, mins)
+
+    suffix = ""
+    if hours <= 12:
+        suffix = "AM"
+    else:
+        suffix = "PM"
+    
+    if (hours == 0):
+        hours = 12
+    
+    if (mins < 10):
+        mins = "0{}".format(mins)
+
+    return "{}:{} {}".format(hours, mins, suffix)
 
 def main():
     customers_n = int(input("How many customers?\n"))
@@ -20,7 +33,7 @@ def main():
     service_start_times  = []
     service_end_times = []
     wait_times = []
-    inactivity_times = []
+    idling_times = []
 
     def generate_between_time():
         return random.randint(0, between_time_max)
@@ -42,17 +55,19 @@ def main():
         service_end_times.append(service_start_times[i] + transaction_times[i])
 
         wait_times.append(service_start_times[i] - arrival_times[i])
-        inactivity_times.append(service_start_times[i] - last_service_end_time)
+        idling_times.append(service_start_times[i] - last_service_end_time)
 
         last_service_end_time = service_end_times[i]
         last_arrival_time = arrival_times[i]
 
-    print(between_times)
-    print(arrival_times)
-    print(transaction_times)
-    print(service_start_times)
-    print(service_end_times)
-    print(wait_times)
-    print(inactivity_times)
+    # printing table
+    spacing = 15
+    row_format = "{customer:<{spacing}}{between:<{spacing}}{arrival:<{spacing}}{transaction:<{spacing}}{start:<{spacing}}{end:<{spacing}}{wait:<{spacing}}{idle}"
+    table_str = row_format.format(customer="Customer", between="Time between", arrival="Arrival", transaction="Transaction", start="Start", end="End", wait="Waiting", idle="ATM idling", spacing=spacing)
+    for i in range(0, customers_n):
+        row_str = row_format.format(customer=i+1, between=between_times[i], arrival=get_time_string(arrival_times[i]), transaction=transaction_times[i], start=get_time_string(service_start_times[i]), end=get_time_string(service_end_times[i]), wait=wait_times[i], idle=idling_times[i], spacing=spacing)
+        table_str += "\n{}".format(row_str)
+    
+    print(table_str)
 
 main()
