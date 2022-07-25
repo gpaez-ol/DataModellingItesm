b3c1b65b1c64e2c782a1cb06c8fe18e36fe8d7d8
Miguel Tornero <a00820449@itesm.mx>
2022-07-24 20:55:26 -0500

    added markov matrix generator

--- ProyectoFinal/MarkovChain.py
+++ ProyectoFinal/MarkovChain.py
@@ -3,6 +3,7 @@ import random
 n = int(input("N?\n"))
 
 markov = []
+warriors = []
 
 def generate_random_markov(n):
     output : list[list[float]] = []
@@ -19,10 +20,27 @@ def generate_random_markov(n):
     return output
 
 def print_matrix(mat):
-    for i in range(0, len(mat)):
+    decimals = 4
+    spacing = decimals + 3
+    rows = len(mat)
+    print("{:<{spacing}}".format("",spacing=spacing), end='')
+    for i in range(0, rows):
+        print("{i:<{spacing}}".format(i=i, spacing=spacing), end='')
+    print("")
+    for i in range(0, rows):
+        print("{i:<{spacing}}".format(i=i, spacing=spacing), end='')
         for j in range(0, len(mat[i])):
-            print("{:4f} ".format(mat[i][j]), end='')
+            print("{value:.{decimals}f} ".format(value=mat[i][j], decimals=decimals), end='')
         print("")
 
+def generate_warriors(n, min, max):
+    output = []
+    for _ in range(0, n):
+        output.append(random.randint(min, max))
+    return output
+
 markov = generate_random_markov(n)
+warriors = generate_warriors(n, 10, 100)
+
 print_matrix(markov)
+print(warriors)
