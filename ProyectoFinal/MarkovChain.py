8c26de1fb6662164f5c8b409da58e8297ef3aef5
Miguel Tornero <a00820449@itesm.mx>
2022-07-26 15:06:28 -0500

    changed group removal logic

--- ProyectoFinal/MarkovChain.py
+++ ProyectoFinal/MarkovChain.py
@@ -5,7 +5,7 @@ n = int(input("N?\n"))
 
 markov = []
 warriors = []
-attack_chances = []
+groups = []
 
 def generate_random_markov(n):
     output : list[list[float]] = []
@@ -21,19 +21,21 @@ def generate_random_markov(n):
             output[i][j] /= row_total
     return output
 
-def print_matrix(mat):
+def matrix_string(mat, groups):
+    output = ""
     decimals = 4
     spacing = decimals + 3
     rows = len(mat)
-    print("{a:<{spacing}}".format(a="",spacing=spacing), end='')
+    output += "{a:<{spacing}}".format(a="",spacing=spacing)
     for i in range(0, rows):
-        print("{i:<{spacing}}".format(i=i, spacing=spacing), end='')
-    print("")
+        output += "{i:<{spacing}}".format(i=groups[i], spacing=spacing)
+    output += '\n'
     for i in range(0, rows):
-        print("{i:<{spacing}}".format(i=i, spacing=spacing), end='')
+        output += "{i:<{spacing}}".format(i=groups[i], spacing=spacing)
         for j in range(0, len(mat[i])):
-            print("{value:.{decimals}f} ".format(value=mat[i][j], decimals=decimals), end='')
-        print("")
+            output += "{value:.{decimals}f} ".format(value=mat[i][j], decimals=decimals)
+        output += '\n'
+    return output
 
 def generate_warriors(n, min, max):
     output = []
@@ -41,53 +43,47 @@ def generate_warriors(n, min, max):
         output.append(random.randint(min, max))
     return output
 
-def remove_state(mat, n):
+def remove_group(mat : list[list[float]], groups : list[int], warriors : list[int], n : int):
+    
+    mat.pop(n)
+    groups.pop(n)
+    warriors.pop(n)
+
     rows = len(mat)
-    columns = len(mat[0])
-    for j in range(0, columns):
-        mat[n][j] = 0.0
+
     for i in range(0, rows):
-        mat[i][n] = 0.0
+        mat[i].pop(n)
+
+    columns = len(mat[0])
+
+    for i in range(0, rows):
         row_sum = sum(mat[i])
         if row_sum > 0.0:
             for j in range(0, columns):
                 mat[i][j] /= row_sum
     return mat
 
-def generate_attack_chances(n):
-    output = []
-    for _ in range(0,n):
-        output.append(1.0 / n)
-    return output
-
-def remove_attack_chance(arr, i):
-    arr[i] = 0.0
-    total = sum(arr)
-    if total > 0:
-        for i in range(0, len(arr)):
-            arr[i] /= total
-    return arr
-
 def choose_from_row(arr):
     output = np.random.choice(a=range(0, len(arr)), size=1, p=arr)
     return output[0]
 
+def generate_groups(n):
+    output = list(range(1,n+1))
+    return output
+
+
 markov = generate_random_markov(n)
 warriors = generate_warriors(n, 10, 100)
-attack_chances = generate_attack_chances(n)
+groups = generate_groups(n)
 
-print_matrix(markov)
+print(matrix_string(markov, groups))
 print(warriors)
-print(attack_chances)
+print(groups)
 
-remove_state(markov, 2)
-print_matrix(markov)
+remove_group(markov, groups, warriors, 2)
+print(matrix_string(markov, groups))
+print(groups)
 
-print(remove_attack_chance(attack_chances, 2))
-
-remove_state(markov, 1)
-print_matrix(markov)
-
-print(remove_attack_chance(attack_chances, 1))
-
-print(choose_from_row(attack_chances))
+remove_group(markov, groups, warriors, 1)
+print(matrix_string(markov, groups))
+print(groups)
